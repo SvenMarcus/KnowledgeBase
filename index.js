b476cc6efb4b7be3ce371dba85da38b48d7cbf86
Sven Marcus <s.marcus@outlook.de>
2020-01-14 15:55:36 +0100

    Switch to express.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT || 5000);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
